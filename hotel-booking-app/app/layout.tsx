58030a1b1b92ddd3a48508b96098cc95da0e936b
frankrodrib-cyber <frankrodrib@gmail.com>
2026-01-16 11:55:30 -0500

    Fix: Aplicado diseño Tailwind y estilos globales

---
+++ hotel-booking-app/app/layout.tsx
@@ -0,0 +1,21 @@
+﻿import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'La Posada de Frank',
+  description: 'Sistema de reservas',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
