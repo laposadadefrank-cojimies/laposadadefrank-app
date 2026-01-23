1d1a180daf1b5fcc73e817b7fde5677848aaedee
frankrodrib-cyber <frankrodrib@gmail.com>
2026-01-23 16:32:01 -0500

    LPDF1

---
+++ hotel-booking-app/next.config.mjs
@@ -0,0 +1,8 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Eliminamos el modo standalone para que Vercel use su configuración nativa
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+export default nextConfig
